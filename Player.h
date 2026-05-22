b4af42ae177751010ae76fbb02d51381103c4250
PA0358\benti <j05825253@gmail.com>
2026-05-22 13:22:53 +0900

    Revert "武器切り替え機能追加"
    This reverts commit e41747819a62b3dd6c3b03b23fa5fd3f4bbfb6bd.

--- Player.h
+++ Player.h
@@ -8,23 +8,12 @@ class PhysicsManager;
 class CollisionWorld;
 class Enemy;
 
-enum class EquipState
-{
-	Unarmed,
-	Weapon1,
-	Weapon2
-};
-
 class Player : public CharacterBase
 {
 public:
 	void Init(CollisionWorld* w);
 	void Update(float dt, float cameraAngle, PhysicsManager& physics);
 	void Draw();
-
-	void EquipWeapon1();
-	void EquipWeapon2();
-	void Unequip();
 
 	void CheckAttackHit(std::vector<std::unique_ptr<Enemy>>& enemies);
 
@@ -60,13 +49,6 @@ private:
 
 	bool attackActive = false;
 	bool attackHit = false;
-
-	// ===== •Ší =====
-	Weapon weapon1;
-	Weapon weapon2;
-
-	// ===== Œ»İ‘•”õ =====
-	EquipState equipState;
 
 	// ===== ˆÚ“®‘¬“x =====
 	float walkSpeed = 220.0f;
